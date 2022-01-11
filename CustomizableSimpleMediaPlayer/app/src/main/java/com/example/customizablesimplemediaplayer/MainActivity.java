e8080d13dcfe50edc4be09d072ae364c5bc28c6d
StudyforM <devdevstudystudy@gmail.com>
2022-01-11 23:15:28 +0900

    add some logics to extract data.
    Note:
    Extract video/audio data.
    Need to add logic to hold smaples in the feature.
    
    Test:
    Run and extract video/audio data correctly.

--- CustomizableSimpleMediaPlayer/app/src/main/java/com/example/customizablesimplemediaplayer/MainActivity.java
+++ CustomizableSimpleMediaPlayer/app/src/main/java/com/example/customizablesimplemediaplayer/MainActivity.java
@@ -6,12 +6,14 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.MediaCodec;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
 import android.os.Bundle;
 import android.util.Log;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
@@ -21,8 +23,23 @@ public class MainActivity extends AppCompatActivity {
     };
 
     // Permission denied
-    private static final String PATH = "/storage/self/primary/DCIM/Camera/XAVCS_4K.MP4";
-    private static final String TAG = "CustomizableSimpleMediaplayer";
+//    private static final String PATH = "/storage/self/primary/DCIM/Camera/XAVCS_4K.MP4";
+    private static final String PATH = "/storage/self/primary/DCIM/Camera/PXL_20201114_014253759.mp4";
+    private static final String TAG = "CSMPlayer";
+    private static final int INPUT_BUFFER_SIZE = 500000;
+    int mVideoSampleSize = 0;
+    int mAudioSampleSize = 0;
+    int mOtherSampleSize = 0;
+    int mVideoIndex = -1;
+    int mAudioIndex = -1;
+    int mOtherIndex = -1;
+    String mVideoFormat = "";
+    String mAudioFormat = "";
+
+    MediaExtractor mExtractor = new MediaExtractor();
+    MediaCodec mVideoCodec = null;
+    MediaCodec mAudioCodec = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,23 +55,83 @@ public class MainActivity extends AppCompatActivity {
                     REQUEST_EXTERNAL_STORAGE
             );
         }
-        // https://developer.android.com/reference/android/media/MediaExtractor
-        MediaExtractor extractor = new MediaExtractor();
+
+        Log.d(TAG, "Extracting ----->");
         try {
-            Log.d(TAG, "Calling setDataSource");
-            extractor.setDataSource(PATH);
+            Log.d(TAG, "setDataSource -->");
+            mExtractor.setDataSource(PATH);
+            Log.d(TAG, "setDataSource <--");
         } catch (IOException e) {
             Log.d(TAG, "In setDataSource, raised exception");
             e.printStackTrace();
         }
 
-        // format[0] = video, format[1] = audio
-        int numTracks = extractor.getTrackCount();
-
+        // TODO:More than 2tracks.
+        // format[0] = video, format[1] = audio or format[0] = audio, format[1] = video
+        Log.d(TAG, "selectTrack -->");
+        int numTracks = mExtractor.getTrackCount();
         for (int i = 0; i < numTracks; i++) {
-            MediaFormat format = extractor.getTrackFormat(i);
-            Log.d(TAG, "format["+ i +"]" + " = " + format);
+            MediaFormat format = mExtractor.getTrackFormat(i);
+            String mime = format.getString(MediaFormat.KEY_MIME);
+            if (mime.startsWith("video/")) {
+                mExtractor.selectTrack(i);
+                mVideoIndex = i;
+                mVideoFormat = mime;
+                try {
+                    mVideoCodec = MediaCodec.createByCodecName(mVideoFormat);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (mime.startsWith("audio/")) {
+                mExtractor.selectTrack(i);
+                mAudioIndex = i;
+                mAudioFormat = mime;
+                try {
+                    mAudioCodec = MediaCodec.createByCodecName(mAudioFormat);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                mExtractor.selectTrack(i);
+                mOtherIndex = i;
+            }
+            Log.d(TAG, "format[" + i + "]" + " = " + format);
         }
-        Log.d(TAG, "setDataSource is end");
+
+        Log.d(TAG, "selectTrack <-- Video=" + mVideoFormat + " Audio=" + mAudioFormat);
+        Log.d(TAG, "readSampleData -->");
+        ByteBuffer inputBuffer = ByteBuffer.allocate(INPUT_BUFFER_SIZE);
+        while (true) {
+            int sampleSize = 0;
+            sampleSize = mExtractor.readSampleData(inputBuffer, 0);
+            if (sampleSize < 0) {
+                Log.d(TAG, "reach EOS, mVideoSampleSize= " + mVideoSampleSize +
+                        "|mAudioSampleSize= " + mAudioSampleSize + "|mOtherSampleSize= " + mOtherSampleSize);
+                break;
+            }
+            if (mExtractor.getSampleTrackIndex() == mVideoIndex) {
+                mVideoSampleSize += sampleSize;
+            } else if (mExtractor.getSampleTrackIndex() == mAudioIndex) {
+                mAudioSampleSize += sampleSize;
+            } else {
+                mOtherSampleSize += sampleSize;
+            }
+            mExtractor.advance();
+        }
+        Log.d(TAG, "readSampleData -->");
+//        mExtractor.release();
+//        mExtractor = null;
+        Log.d(TAG, "Extracting <-----");
+
+        Log.d(TAG, "Decode ----->");
+        Log.d(TAG, "Decode <-----");
     }
+
+    // TODO: keep samples to readSampleData()
+    public class SampleHolder {
+        SampleHolder() {
+
+        }
+    }
+
 }
